7d7830019c6f8bcb910c378a32b4b6ccefe88292
panzeyu2013 <1971614652@qq.com>
2024-04-14 22:49:30 +0800

    ALU_verify_complete_need_check

--- test/verify/ALU.py
+++ test/verify/ALU.py
@@ -10,19 +10,26 @@ def is_zero(number):
     else:
         return 0
 
-def is_overflow(number):
-    if number >= 2**31 - 1 or number <= -2**31:
-        return 1
+def is_overflow(number,is_signed):
+    if is_signed:
+        if number > 2**31 - 1 or number < - 2**31:
+            return 1
+        else:
+            return 0
     else:
-        return 0
+        if number > 2**32 - 1 or number < 0:
+            return 1
+        else:
+            return 0
+
     
 def verify(A,B,op,shift_cout):
     if op == 0:
         F = A & B
         N = get_sign(F)
         Z = is_zero(F)
-        C = shift_cout
-        V = None
+        C = shift_cout # 无符号运算溢出
+        V = None #有符号运算溢出
         return F,N,Z,C,V
     elif op == 1:
         F = A ^ B
@@ -35,68 +42,77 @@ def verify(A,B,op,shift_cout):
         F = A - B
         N = get_sign(F)
         Z = is_zero(F)
-        C = is_overflow(F)
-        # V = 
-        
+        C = is_overflow(F,False)
+        V = is_overflow(F,True)
+        return F,N,Z,C,V
     elif op == 3:
-        pass
+        F = B - A
+        N = get_sign(F)
+        Z = is_zero(F)
+        C = is_overflow(F,False)
+        V = is_overflow(F,True)
+        return F,N,Z,C,V
     elif op == 4:
-        pass
+        F = A + B
+        N = get_sign(F)
+        Z = is_zero(F)
+        C = is_overflow(F,False)
+        V = is_overflow(F,True)
+        return F,N,Z,C,V
     elif op == 5:
-        pass
+        F = A + B + shift_cout
+        N = get_sign(F)
+        Z = is_zero(F)
+        C = is_overflow(F,False)
+        V = is_overflow(F,True)
+        return F,N,Z,C,V
     elif op == 6:
-        pass
+        F = A - B + shift_cout - 1
+        N = get_sign(F)
+        Z = is_zero(F)
+        C = is_overflow(F,False)
+        V = is_overflow(F,True)
+        return F,N,Z,C,V
     elif op == 7:
-        pass
+        F = B - A + C - 1
+        N = get_sign(F)
+        Z = is_zero(F)
+        C = is_overflow(F,False)
+        V = is_overflow(F,True)
+        return F,N,Z,C,V
     elif op == 8:
         return A,None,None,None,None
     elif op == 9:
         pass
     elif op == 10:
-        pass
+        F = A - B + 4
+        N = get_sign(F)
+        Z = is_zero(F)
+        C = is_overflow(F,False)
+        V = is_overflow(F,True)
+        return F,N,Z,C,V
     elif op == 11:
         pass
-    
     elif op == 12:
         F = A | B
-        if F >= 0:
-            N = 0
-        else:
-            N = 1
-        if F == 0:
-            Z = 1
-        else:
-            Z = 0
+        N = get_sign(F)
+        Z = is_zero(F)
         C = shift_cout
         V = None
         return F,N,Z,C,V
     elif op == 13:
         return B,None,None,None
-    
     elif op == 14:
         F = A & (~B)
-        if F >= 0:
-            N = 0
-        else:
-            N = 1
-        if F == 0:
-            Z = 1
-        else:
-            Z = 0
+        N = get_sign(F)
+        Z = is_zero(F)
         C = shift_cout
         V = None
         return F,N,Z,C,V
-    
     elif op == 15:
         F = ~B
-        if F >= 0:
-            N = 0
-        else:
-            N = 1
-        if F == 0:
-            Z = 1
-        else:
-            Z = 0
+        N = get_sign(F)
+        Z = is_zero(F)
         C = shift_cout
         V = None
         return F,N,Z,C,V
