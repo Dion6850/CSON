485b283610c3e56d0548800d4f85798ba8e3d160
Dion <1079894021@qq.com>
2024-06-05 16:05:01 +0800

    find bug

--- src/Memory.v
+++ src/Memory.v
@@ -7,18 +7,18 @@ module memory(
     input [31:0] m_w_data,
     output [31:0] m_r_data
 );
-    reg [31:0]mem [127:0];
-    wire [31:0] mem0 [31:0];
-    wire [31:0] mem1 [31:0];
-    wire [31:0] mem2 [31:0];
-    wire [31:0] mem3 [31:0];
+    reg [31:0]mem [0:127];
+    wire [31:0] mem0 [0:31];
+    wire [31:0] mem1 [0:31];
+    wire [31:0] mem2 [0:31];
+    wire [31:0] mem3 [0:31];
 genvar i;
 generate
     for(i = 0 ;i < 32 ; i = i + 1)begin
         assign mem0[i] = mem[i];
-        assign mem1[i] = mem[i+31];
-        assign mem2[i] = mem[i+63];
-        assign mem3[i] = mem[i+95]; 
+        assign mem1[i] = mem[i+32];
+        assign mem2[i] = mem[i+64];
+        assign mem3[i] = mem[i+96]; 
     end
 endgenerate
     reg [31:0] inner;
